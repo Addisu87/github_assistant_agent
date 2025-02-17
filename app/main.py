cde0d6d72ea1a1cf727b67d19041e2bffbd196ee
Addisu <addisuhaile87@gmail.com>
2025-02-17 18:37:40 +0300

    Change routes and folder structure

--- app/main.py
+++ app/main.py
@@ -3,7 +3,7 @@ from pathlib import Path
 
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from routes.agent_endpoint import router as agent_router
+from routers.agent_endpoint import router as agent_router
 
 app = FastAPI()
 
